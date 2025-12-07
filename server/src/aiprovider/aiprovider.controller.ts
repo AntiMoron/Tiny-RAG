6a58509d4b8829c48005751ebb5b410a7e63ce55
Antimoron <anti2moron@gmail.com>
2025-12-07 14:05:09 +0800

    chore: start to use pnpm

--- server/src/aiprovider/aiprovider.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('aiprovider')
-export class AiproviderController {}
--- server/src/embedding/stopWords.ts
+++
@@ -1,1 +0,0 @@
-export default ['a', 'the', 'de', '啊', '了', '是', '哈'];
