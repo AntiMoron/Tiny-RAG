bf726d67b557c1b3736ad8fc0875a8d1d8bd33b3
Antimoron <anti2moron@gmail.com>
2025-12-12 23:15:14 +0800

    fix

--- packages/server/src/retrieve/retrieve.module.ts
+++ packages/server/src/retrieve/retrieve.module.ts
@@ -5,11 +5,18 @@ import { VectorDbModule } from 'src/vector-db/vector-db.module';
 import { ChunkModule } from 'src/chunk/chunk.module';
 import { RetrieveController } from './retrieve.controller';
 import { DatasetModule } from 'src/dataset/dataset.module';
+import { CompletionModule } from 'src/completion/completion.module';
 
 @Module({
   providers: [RetrieveService],
   exports: [RetrieveService],
-  imports: [EmbeddingModule, VectorDbModule, ChunkModule, DatasetModule],
+  imports: [
+    EmbeddingModule,
+    VectorDbModule,
+    ChunkModule,
+    DatasetModule,
+    CompletionModule,
+  ],
   controllers: [RetrieveController],
 })
 export class RetrieveModule {}
